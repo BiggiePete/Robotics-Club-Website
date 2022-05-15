97faf17e8828a1d0e00ec909db2ec457b9fbe6ff
BiggiePete <pter.cross222@gmail.com>
2022-05-15 02:32:04 -0400

    prrof of concept that we can load HUGE models

--- src/client/scripts/modelviewer.ts
+++ src/client/scripts/modelviewer.ts
@@ -5,7 +5,7 @@ import * as THREE from 'three'
 import { decideAA } from './experience_helperfuncs'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
-import { LoadingManager } from 'three';
+import { LoadingManager, Object3D } from 'three';
 
 
 
@@ -30,9 +30,10 @@ const loadingManager = new THREE.LoadingManager(() => {
 const FBXloader = new FBXLoader(loadingManager);
 
 function LoadObject(o: string) {
-    o = './Media/3dmodels/' + o + '.glb';
+    o = './Media/3dmodels/' + o + '.fbx';
     FBXloader.load(o, function (object) {
         object.name = o.slice(o.lastIndexOf("/") + 1, o.lastIndexOf("."));
+        object.scale.multiplyScalar(0.01)
         scene.add(object);
     });
 }
@@ -46,25 +47,43 @@ Email : peter.cross222@gmail.com
 */
 
 //vars to play with : 
+var FinalFullBuildv56:Object3D
 
+//get the data from the script tag we live in : 
+const objectname = document.currentScript?.outerHTML.slice(document.currentScript?.outerHTML.lastIndexOf('=') + 2, document.currentScript?.outerHTML.lastIndexOf('"'));
 
 //Model to Load : 
-//LoadObject()
+console.log(objectname);
+LoadObject(objectname ? objectname : "") // i hate this
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const clock = new THREE.Clock();
 const renderer = new THREE.WebGLRenderer({
     antialias: decideAA(),
-    powerPreference: "high-performance"
+    powerPreference: "high-performance",
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({
+    color: 0xa7cca7,
+    wireframe: true,
+});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 //use the pointer lock controls to make a good fps camera controller
-
+const ambientLight = new THREE.AmbientLight()
+scene.add(ambientLight)
+const light = new THREE.PointLight()
+light.position.set(0.8, 1.4, 1.0)
+scene.add(light)
 
 camera.position.z = 5;
 
@@ -72,6 +91,7 @@ function animate(): void {
     requestAnimationFrame(animate);
     controls.update();
     const delta = clock.getDelta();
+    console.log(scene.getObjectByName("FinalFullBuildv56"));
 
     renderer.render(scene, camera);
 };
